4fccf9300d9df748e3bc6dc9fd6c679606711586
Vitond <Vitond@github.com>
2022-05-17 16:17:08 +0200

    Remove CZC

--- src/pages/Sponsors/Sponsors.tsx
+++ src/pages/Sponsors/Sponsors.tsx
@@ -1,7 +1,6 @@
 import classes from './Sponsors.module.scss';
 import Heading from '../../components/typography/Heading';
 import { headingTypes } from '../../types/types';
-import CZCLogo from "../../assets/CZC_logo.png";
 import { routeTransition, routeVariants } from "../../animations/animations";
 import Section from '../../components/layout/Section/Section';
 import { motion } from 'framer-motion';
@@ -10,9 +9,9 @@ const Sponsors = () => {
     return <motion.div className={classes.Sponsors} variants={routeVariants} transition={routeTransition} exit="hidden" animate="visible" initial="initial">
         <Section className={''}>
             <Heading className={''} type={headingTypes.main}>Sponzoři</Heading>
-            <div className={classes.Sponsors__sponsor}>
+            {/* <div className={classes.Sponsors__sponsor}>
                 <img className={classes.Sponsors__sponsor__logo} src={CZCLogo}></img>
-            </div>
+            </div> */}
         </Section>
     </motion.div>
 };
