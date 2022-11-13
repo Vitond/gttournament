d9f3f4e6533a141aab6734115ae35673c514e5b2
Ondřej Vitula <vitula@tectronik.cz>
2022-11-13 18:41:59 +0100

    Add Artin, Update Valorant Rules

--- src/pages/Sponsors/Sponsors.tsx
+++ src/pages/Sponsors/Sponsors.tsx
@@ -3,15 +3,16 @@ import Heading from '../../components/typography/Heading';
 import { headingTypes } from '../../types/types';
 import { routeTransition, routeVariants } from "../../animations/animations";
 import Section from '../../components/layout/Section/Section';
+import ArtinLogo from '../../assets/artinlogo.png';
 import { motion } from 'framer-motion';
 
 const Sponsors = () => {
     return <motion.div className={classes.Sponsors} variants={routeVariants} transition={routeTransition} exit="hidden" animate="visible" initial="initial">
         <Section className={''}>
             <Heading className={''} type={headingTypes.main}>Sponzoři</Heading>
-            {/* <div className={classes.Sponsors__sponsor}>
-                <img className={classes.Sponsors__sponsor__logo} src={CZCLogo}></img>
-            </div> */}
+            <div className={classes.Sponsors__sponsor}>
+                <a href="https://www.artin.cz/"><img className={classes.Sponsors__sponsor__logo} src={ArtinLogo}></img></a>
+            </div>
         </Section>
     </motion.div>
 };
