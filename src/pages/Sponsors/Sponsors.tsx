bda96c76a9e6dd34bcddfb1cf6393cd655993787
Ondřej Vitula <vitula@tectronik.cz>
2023-09-26 12:17:30 +0200

    Chage Sponsors Page

--- src/pages/Sponsors/Sponsors.tsx
+++ src/pages/Sponsors/Sponsors.tsx
@@ -3,16 +3,15 @@ import Heading from '../../components/typography/Heading';
 import { headingTypes } from '../../types/types';
 import { routeTransition, routeVariants } from "../../animations/animations";
 import Section from '../../components/layout/Section/Section';
-import ArtinLogo from '../../assets/artinlogo.png';
+import SponsorshipPackages from '../../assets/sponzorske-balicky.png';
 import { motion } from 'framer-motion';
 
 const Sponsors = () => {
     return <motion.div key="sponsors" className={classes.Sponsors} variants={routeVariants} transition={routeTransition} exit="hidden" animate="visible" initial="initial">
         <Section className={''}>
             <Heading className={''} type={headingTypes.main}>Sponzoři</Heading>
-            <div className={classes.Sponsors__sponsor}>
-                <a href="https://www.artin.cz/"><img className={classes.Sponsors__sponsor__logo} src={ArtinLogo}></img></a>
-            </div>
+            <img className={classes.Sponsors__packages} src={SponsorshipPackages}></img>
+            <p className={classes.Sponsors__contact}>Pokud máte zájem stát se sponzorem, kontaktujte nás na <b><a href="mailtoturnajvpocitacovychhrach@gym-tisnov.cz">turnajvpocitacovychhrach@gym-tisnov.cz</a></b>.</p>
         </Section>
     </motion.div>
 };
